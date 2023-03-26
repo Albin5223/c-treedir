dc69ead76b3aed04f5e971aab33b3c9a10465f4e
paris <paris.albin23@gmail.com>
2023-03-26 10:23:31 +0200

    gestion des chemins

--- src/chemin.h
+++ src/chemin.h
@@ -4,14 +4,54 @@
 
 
 
-char *couper(char *s1){
-    char *res="";
+/**
+ * Cette fonction renvoie la longueur du mot qui se trouve avant le prochain /
+ * @param s1 est le chemin qu'on veut decomposer
+ * @return La longueur du mot avant /
+*/
+size_t longueur(char *s1){
     size_t i = 0;
     while(*(s1+i) != '/' && *(s1+i) != '\0'){
-        res = strcat(res,s1+i);
+        
         i++;
-
     }
+    return i;
+}
 
-    return res;
+
+/**
+ * Cette fonction renvoie le mot de 0 a lon
+ * @param s1 est le mot dont on veut le début
+ * @param lon est la longueur du sous-mot
+ * @return L'emplacement du sous mot !!! ATTENTION : Il faut free la valeur de retour
+*/
+
+char *recuperer(char *s1,size_t lon){
+    char *tab =malloc((lon+1)*sizeof(char));
+
+    for(size_t i = 0;i<lon;i++){
+        *(tab+i) = *(s1+i);
+    }
+    *(tab+lon)='\0';
+
+    return tab;
+}
+
+
+/**
+ * Cette fonction renvoie le nombre de / dans s1
+ * @param s1 est le mot dont on veut le début
+ * @return size_t le nombre de / dans s1
+*/
+
+size_t nbSlash(char *s1){
+    size_t compt = 0;
+    size_t i = 0;
+    while(*(s1+i) != '\0'){
+        if(*(s1+i)=='/'){
+            compt++;
+        }
+        i++;
+    }
+    return compt;
 }
