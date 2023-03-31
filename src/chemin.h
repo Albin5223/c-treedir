2b24b04667fe3da500d4ed4f080d0f6bd3e2be10
paris <paris.albin23@gmail.com>
2023-03-31 12:08:13 +0200

    Modification des fichiers

--- src/chemin.h
+++ src/chemin.h
@@ -3,56 +3,8 @@
 #include<string.h>
 
 
+size_t nbSlash(char *s1);
+size_t longueur(char *s1);
+char *recuperer(char *s1,size_t lon);
 
-/**
- * Cette fonction renvoie la longueur du mot qui se trouve avant le prochain /
- * @param s1 est le chemin qu'on veut decomposer
- * @return La longueur du mot avant /
-*/
-size_t longueur(char *s1){
-    size_t i = 0;
-    while(*(s1+i) != '/' && *(s1+i) != '\0'){
-        
-        i++;
-    }
-    return i;
-}
-
-
-/**
- * Cette fonction renvoie le mot de 0 a lon
- * @param s1 est le mot dont on veut le début
- * @param lon est la longueur du sous-mot
- * @return L'emplacement du sous mot !!! 
- * @warning Il faut free la valeur de retour
-*/
-char *recuperer(char *s1,size_t lon){
-    char *tab =malloc((lon+1)*sizeof(char));
-
-    for(size_t i = 0;i<lon;i++){
-        *(tab+i) = *(s1+i);
-    }
-    *(tab+lon)='\0';
-
-    return tab;
-}
-
-
-/**
- * Cette fonction renvoie le nombre de / dans s1
- * @param s1 est le mot dont on veut le début
- * @return size_t le nombre de / dans s1
-*/
-
-size_t nbSlash(char *s1){
-    size_t compt = 0;
-    size_t i = 0;
-    while(*(s1+i) != '\0'){
-        if(*(s1+i)=='/'){
-            compt++;
-        }
-        i++;
-    }
-    return compt;
-}
 
